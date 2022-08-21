3a35f3eb5c8395dea46309928717717ae7cf05f5
IrynaPet <i0988239873@gmail.com>
2022-08-21 23:00:14 +0100

    232323

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,39 +1,31 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-  
-// 1. відкриваємо достум до кнопок і боді!!!
-const body = document.querySelector("body");
-const btnStart = document.querySelector("button[data-start]");
-const btnStop = document.querySelector("button[data-stop]");
 
-// 2. Оголошуємо змінну таймера!
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
 let timerId = null;
 
-// 3. Встановлюємо початкове значення кнопки Стоп!
 btnStop.disabled = true;
 
-// 4. Прослуховуємо кнопки!!
-btnStart.addEventListener("click",onStart);
-btnStop.addEventListener("click", onStop);
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop);
 
-// 5. Запускаємо зміну кольорів
-function onStart () {
-    btnStart.disabled = true,
-    btnStop.disabled = false,
-    timerId = setInterval(() => {
-    body.style.background = getRandomHexColor()
-    },1000)
-};
+function onStart() {
+  (btnStart.disabled = true),
+    (btnStop.disabled = false),
+    (timerId = setInterval(() => {
+      body.style.background = getRandomHexColor();
+    }, 1000));
+}
 
-// 6. Зупиняємо зміну кольорів
-function onStop(){
-    if(timerId) {
-        clearInterval(timerId);
-    btnStart.disabled = false
-    btnStop.disabled = true
-    }
-    return
-};
-
-
+function onStop() {
+  if (timerId) {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+  }
+  return;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,45 +1,40 @@
-// 1. Імпорт бібліотеки вспливаючого вікна
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// 2. Відкриття доступу до елементів
-const form = document.querySelector(".form");
-const delayInput = form.querySelector("input[name = delay]");
-const stepInput = form.querySelector("input[name = step]");
-const amountInput = form.querySelector("input[name = amount]");
+const form = document.querySelector('.form');
+const delayInput = form.querySelector('input[name = delay]');
+const stepInput = form.querySelector('input[name = step]');
+const amountInput = form.querySelector('input[name = amount]');
 
-// 3. Прослуховування подій на кнопці
-form.addEventListener("submit", onSubmitForm);
+form.addEventListener('submit', onSubmitForm);
 
-// 4. Створення помісу функції
 function createPromise(position, delay) {
   const myPromis = new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
-      if(shouldResolve) {
-        resolve({position,delay});
-      } 
-        reject({position,delay});
-    },delay);
-  })
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
   return myPromis;
-};
-// 5. Запобігання пеезавантаженню вікна
+}
+
 function onSubmitForm(event) {
   event.preventDefault();
 
-// 6. Оголошення змінних
-  let delayValue = + delayInput.value;
-  let stepValue = + stepInput.value;
-  let amountValue = + amountInput.value + 1;
+  let delayValue = +delayInput.value;
+  let stepValue = +stepInput.value;
+  let amountValue = +amountInput.value + 1;
 
-  for (let i = 1 ; i < amountValue; i++){
-    createPromise(i,delayValue)
-    .then(({position,delay}) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-    })
-    .catch(({position,delay}) => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-    })
-   delayValue += stepValue
+  for (let i = 1; i < amountValue; i++) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    delayValue += stepValue;
   }
-};
+}
